0891b94e8f9840325dc52b2b1bd9c939a3b91d33
sakibdevlekar <sakibdevlekar96@gmail.com>
2023-07-10 09:33:07 +0530

    sakib

--- controller/team.controller.js
+++ controller/team.controller.js
@@ -18,7 +18,7 @@ const teamObj ={
 
     temp:"inviteLink"+"/"+req.body.AdminID+"/"+req.body.teamName+"/"+req.body.teamAdminUID,
 }
-    teamObj. inviteLink =req.protocol +"://"+req.hostname+"/"+teamObj.temp.replace(/\\/g, "/");
+    teamObj. inviteLink =req.protocol +":8000//"+req.hostname+"/"+teamObj.temp.replace(/\\/g, "/");
 try{
     const admin = await TeamAdmin.findOne({UID:req.body.teamAdminUID});
     if(!admin){
